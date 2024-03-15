001e0ba8964e02e18bbf3c007bc07738485601a4
zyad <zyadhimself1@gmail.com>
2024-03-15 15:03:55 +0200

    handled an error which occurs when the length of the block is even and modified the xorString to make sure the text only contains allowed chars

--- src/main/Feistel.java
+++ src/main/Feistel.java
@@ -25,7 +25,8 @@ public class Feistel{
     private static String xorStrings(String a, String b){
         StringBuilder str = new StringBuilder();
         for(int i =0;i<a.length();i++){
-            str.append((char) (a.charAt(i)^(b.charAt(i))));
+            str.append((char) (((a.charAt(i)^(b.charAt(i)))%95)+33)); // makes sure the resulting char falls in within the printable ascii
+                                                                                                                        //after space
         }
         return str.toString();
     }
@@ -33,7 +34,7 @@ public class Feistel{
     /**
      *this is the f function in the Feistel algorithm
      *takes one half of the block to encrypt it using simple xor then return it
-     * @param block
+     * @param block the half block from Feistel
      * @return the Xored half of the Feistel's block
      */
     public static String func(String block){
@@ -43,12 +44,15 @@ public class Feistel{
 
     /**
      * @param block look up Feistel networks
-     * @param numOfRounds
+     * @param numOfRounds the number of rounds in Feistel network usually *16*
      * @return the encrypted block
      */
     public static String encrypt(String block, int numOfRounds){
         String left = block.substring(0,block.length()/2);
         String right =  block.substring(block.length()/2);
+        if(left.length() != right.length()){ // to prevent overflow in xorStrings
+            left = left + "x";
+        }
         for(int i= 0;i<numOfRounds;i++){
             String temp = right;
             right = xorStrings(left,func(right));
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println(Vigenere.encrypt("I've been through the desert on a horse with no name "));
         System.out.println(PlayfairCipher.encrypt("In the desert, you can't remember your name"));
         System.out.println(Vigenere.decrypt(Vigenere.encrypt("Cause there ain't no one for to give you no pain")));
+        System.out.println(Feistel.encrypt("let's go get some shit done",16));
     }
 }
